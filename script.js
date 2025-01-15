2375a56e2451b6d8070d37f48bd191e6b43072f6
devanshtrivedi2203 <119436025+devanshtrivedi2203@users.noreply.github.com>
2025-01-15 19:37:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript to add scroll effect to the navbar
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
